70c551c27db291564ada2d006d6631e5d5f0f523
Andria-Hibe <andriacohibe@gmail.com>
2019-10-13 22:00:11 +1300

    Add calculation functionality

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const calculator = {
-  value: "0"
-}
-
-function inputDigit(digit) {
-  const value = calculator.value;
-  calculator.value = value === '0' ? digit : value + digit;
+  value: "0",
+  firstOperand: null,
+  secondOperandReady: false,
+  operator: null
 }
 
 function updateDisplayValue() {
@@ -14,11 +12,58 @@ function updateDisplayValue() {
 
 updateDisplayValue();
 
+function clearCalculator(){
+  calculator.value = '0';
+  calculator.firstOperand = null;
+  calculator.secondOperandReady = false;
+  calculator.operator = null;
+}
+
+function inputDigit(digit) {
+  const {value, secondOperandReady} = calculator;
+
+  if (secondOperandReady === true) {
+    calculator.value = digit;
+    calculator.secondOperandReady = false;
+  } else {
+    calculator.value = value === '0' ? digit : value + digit;
+  }
+}
+
 function inputDecimal(decimal) {
   if (!calculator.value.includes(decimal)) {
     calculator.value += decimal;
   }
 }
+
+function inputOperator(clickedOperator) {
+  const {value, firstOperand, operator} = calculator;
+  const inputValue = parseFloat(value);
+
+  if (operator && calculator.secondOperandReady)  {
+    calculator.operator = clickedOperator;
+    return;
+  }
+
+  if (firstOperand === null) {
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const calculation = calculate[operator](firstOperand, inputValue);
+    
+    calculator.value = String(calculation);
+    calculator.firstOperand = calculation;
+  }
+
+  calculator.secondOperandReady = true;
+  calculator.operator = clickedOperator;
+}
+
+const calculate = {
+  '+': (firstOperand, secondOperandReady) => firstOperand + secondOperandReady,
+  '-': (firstOperand, secondOperandReady) => firstOperand - secondOperandReady,
+  '*': (firstOperand, secondOperandReady) => firstOperand * secondOperandReady,
+  '/': (firstOperand, secondOperandReady) => firstOperand / secondOperandReady,
+};
 
 const keys = document.querySelector('.calculator-buttons');
 keys.addEventListener('click', (event) => {
@@ -29,17 +74,14 @@ keys.addEventListener('click', (event) => {
   }
 
   if (target.classList.contains('operator')) {
-    console.log('operator', target.value);
+    inputOperator(target.value);
+    updateDisplayValue();
     return;
   }
-
-  if (target.classList.contains('digit')) {
-    console.log('digit', target.value);
-    return;
-  }
-
+  
   if (target.classList.contains('clear')) {
-    console.log('clear', target.value);
+    clearCalculator();
+    updateDisplayValue();
     return;
   }
 
